232bb5eb57428870e3eb3b3ee002868b44a885cb
cde-la-r <cde-la-r@student.42.fr>
2024-11-05 09:25:15 +0100

    plot

---
+++ codigo/plot_system.m
@@ -0,0 +1,12 @@
+% plot_system.m
+
+function plot_system(x, t, psi_evol, title)
+    figure;
+    for j = 1:length(t)
+        plot(x, abs(psi_evol(:, j)), 'LineWidth', 1.5);
+        title([title, ' - Tiempo t = ', num2str(t(j))]);
+        xlabel('Posición x');
+        ylabel('|\Psi(x,t)|');
+        pause(0.1);
+    end
+end
